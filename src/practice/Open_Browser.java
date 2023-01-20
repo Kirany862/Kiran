21363b9d18d019853912e6bf8b9c6566cbd67fc8
Kirany862 <123139791+Kirany862@users.noreply.github.com>
2023-01-20 13:32:14 +0530

    Update Open_Browser.java

--- src/practice/Open_Browser.java
+++ src/practice/Open_Browser.java
@@ -8,6 +8,7 @@ public class Open_Browser {
 	public static void main(String[] args) {
 	System.setProperty("webdriver.chrome.driver","C:\\Users\\KIRAN\\Downloads\\chromedriver_win32\\chromedriver.exe");	
 	WebDriver driver = new ChromeDriver();
+		System.out.println("Hi");
 	}
 
 }
